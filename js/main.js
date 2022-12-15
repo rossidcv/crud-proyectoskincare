300aa2fc004c17acf3b3508fa7a99f765bdb615d
Monica Rossi <monicarossi3@hotmail.com>
2022-12-15 03:10:34 -0300

    Crud Proyecto final Skincare

---
+++ js/main.js
@@ -0,0 +1,85 @@
+if (document.getElementById("app")) {
+    const { createApp } = Vue
+ 
+    createApp ({
+        data() {
+            return {
+                url: "https://monicadcv83.pythonanywhere.com/productos",
+                productosAll: [],
+                productos: [],
+                errored: false,
+                loading: true,
+                tratamientos:[],
+                precios: [],
+                tratamiento: "All",
+                precio: "All"
+            }
+        },
+        methods: {
+            /*fetchData(url) {
+                fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    this.productos = data;
+                    this.loading = false;
+                })
+                .catch(err => {
+                    this.errored = true
+                })
+            },*/
+            async fetchData(url) {  // necesite un async y await porque seguia de largo y no me cargaba el array productos ni las listas desplegables
+                fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    this.productos = data;
+                    this.loading = false;
+                })
+                .catch(err => {
+                    this.errored = true
+                })
+                resp = await fetch(url)
+                this.productosAll = await resp.json() 
+                this.productos = this.productosAll
+            },
+            eliminar(producto) {
+                const url = 'https://monicadcv83.pythonanywhere.com/productos/' + producto;
+                var options = {
+                    method: 'DELETE',
+                }
+                fetch(url, options)
+                    .then(res => res.text()) // or res.json()
+                    .then(res => {
+                        location.reload();
+                    })                    
+                    console.log("eliminado")
+                    alert("Se eliminará este producto")
+                    window.location.href = "./listaproductos.html";  //NUEVO 
+                    },
+            filtro() {
+                this.productos = this.productosAll.filter( producto=>(producto.nombre == this.nombre || this.nombre==="All") && (producto.precio == this.precio || this.precio==="All" ))                
+            },
+            orden() {
+                this.productos.sort((a, b) => { return (a.precio > b.precio ? 1 : -1) } )// si retorna 1 lo invierte, si retorna -1 lo deja como esta 
+            },
+            cargarListasDesplegables() {
+                this.nombres =['All']
+                this.precios = ['All']
+                for (producto of this.productosAll) {
+                  if (producto.nombre !== '' && this.nombres.indexOf(producto.nombre) < 0) {
+                    this.nombres.push(producto.nombre)
+                  }
+                  if (producto.precio !== '' && this.precios.indexOf(producto.precio) < 0) {
+                    this.precios.push(producto.precio)
+                  }
+                }
+            }
+        },
+        created() {
+            this.fetchData(this.url)
+        },
+        async created() {
+            await this.fetchData(this.url)
+            this.cargarListasDesplegables()
+        }
+    }).mount('#app')
+}
